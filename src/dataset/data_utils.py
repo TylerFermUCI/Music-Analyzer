0bef9375f8f6fbf76f000250c3c6efa41e7199c3
Tyler Fermanian <tfermani@uci.edu>
2023-12-22 17:05:51 -0800

    Add csv_stats function

--- src/dataset/data_utils.py
+++ src/dataset/data_utils.py
@@ -32,7 +32,7 @@ def create_csv(data_path: str, new_file_path: str) -> None:
         # Create a new file.
         with open(os.path.join(root, new_file_path), 'w', newline='') as file:
             writer = csv.writer(file)
-            field = ["song_id", "mood", "valence", "arousal", "difference", "temp"]
+            field = ["song_id", "mood", "valence", "arousal"]
             writer.writerow(field)
 
             # Open the data into a dataframe.
@@ -50,7 +50,7 @@ def create_csv(data_path: str, new_file_path: str) -> None:
                     else: mood = "tense"
                 # Write the information into the new csv file.
                 difference = abs(valence - arousal)
-                writer.writerow([df.iloc[i, 0], mood, valence, arousal, difference, valence*difference/arousal])
+                writer.writerow([df.iloc[i, 0], mood, valence, arousal])
 
     # If it doesn't, don't do anything.
     else:
@@ -60,6 +60,7 @@ def create_csv(data_path: str, new_file_path: str) -> None:
 def create_csv_with_inputs(data_path: str, new_file_path: str) -> None:
     """
     Manually assign mood values to songs in the data_path.
+    Useful if you want to assign mood values to the song yourself.
 
     Arguments:
         data_path (str): path to where the data is located.
@@ -120,9 +121,47 @@ def display_csv(csv_file_path: str) -> None:
         print("File doesn't exist") 
 
 
+def csv_stats(csv_file_path: str) -> None:
+    """
+    Print various stats about the csv_file provided (assuming that the file has the headers required).
+
+    Required headers:
+        song_id, mood, valence, arousal
+    
+    Arguments:
+        csv_file_path (str): Path to the csv file.
+    
+    Return value: None
+    """
+    # Merge csv_file_path with root.
+    new_file_path = os.path.join(root, csv_file_path)
+    
+    # Check if path exists.
+    if os.path.exists(new_file_path):
+        # Open the data into a dataframe.
+        df = pd.read_csv(new_file_path)
+        
+        # Check for the right header.
+        if df.columns.values.tolist() == ['song_id', 'mood', 'valence', 'arousal']:
+            # Seperate data frame by mood.
+            df_mood = df.groupby('mood')
+
+            # Print stats about groupings.
+            print("\nData Stats:")
+            print(f"There are {len(df_mood.get_group('happy'))} happy songs.")
+            print(f"There are {len(df_mood.get_group('sad'))} sad songs.")
+            print(f"There are {len(df_mood.get_group('tense'))} tense songs.")
+            print(f"There are {len(df_mood.get_group('calm'))} calm songs.\n")
+        # Incorrect csv file.
+        else:
+            print(f"CSV file given [{csv_file_path}] does not have all the required headers of ['song_id', 'mood', 'valence', 'arousal'].")
+    # If it doesn't, don't do anything.
+    else:
+        print("File doesn't exist")
 
 if __name__ == "__main__":
     csv_path = "data/DEAM_Annotations/annotations averaged per song/song_level/static_annotations_averaged_songs_1_2000.csv"
-    new_csv_path = "data/mood_v2.csv"
+    new_csv_path = "data/mood.csv"
     # display_csv(csv_path)
-    create_csv_with_inputs(csv_path, new_csv_path)
+    # create_csv(csv_path, new_csv_path)
+    csv_stats(new_csv_path)
